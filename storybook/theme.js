c99ca221631d29f654bf9a2e7bde88d087d18337
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-01-04 12:23:24 -0500

    Delete storybook directory

--- storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'dark',
-  brandTitle: 'OpenDEX storybook',
-  brandUrl: 'https://opendex.network',
-  brandImage: 'https://gblobscdn.gitbook.com/spaces%2F-Lq7dAvRcXVJXcPrZrNF%2Favatar.png?alt=media',
-});
