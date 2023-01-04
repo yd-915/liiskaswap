24b86122e459ee4a28d7e310e6befa3f86dec47c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-01-04 12:29:25 -0500

    Add files via upload

---
+++ storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'OpenDEX storybook',
+  brandUrl: 'https://opendex.network',
+  brandImage: 'https://gblobscdn.gitbook.com/spaces%2F-Lq7dAvRcXVJXcPrZrNF%2Favatar.png?alt=media',
+});
